f99b5a8dec00d8e0addaadf0ce93d85286ad3b59
Murad Yusufov <flood4life@gmail.com>
2017-12-25 09:10:29 +0300

    Add CustomerValidator implementation

--- src/customer_validator.rb
+++ src/customer_validator.rb
@@ -1,2 +1,18 @@
+require 'dry-validation'
+
 class CustomerValidator
+  class << self
+    def call(customer)
+      SCHEMA.call(customer).success?
+    end
+
+    private
+
+    SCHEMA = Dry::Validation.Schema do
+      required(:name).filled(:str?)
+      required(:user_id).filled(:int?, gt?: 0)
+      required(:latitude) { filled? & (int? | float?) & gteq?(-90) & lteq?(90) }
+      required(:longitude) { filled? & (int? | float?) & gteq?(-180) & lteq?(180) }
+    end
+  end
 end
