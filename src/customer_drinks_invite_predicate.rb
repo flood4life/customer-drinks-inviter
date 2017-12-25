14200f444a69211758bce242d23865d3fcfc7ffd
Murad Yusufov <flood4life@gmail.com>
2017-12-25 09:50:37 +0300

    Add implementation for CustomerDrinksInvitePredicate

--- src/customer_drinks_invite_predicate.rb
+++ src/customer_drinks_invite_predicate.rb
@@ -1,2 +1,21 @@
 class CustomerDrinksInvitePredicate
+  def initialize(**options)
+    @max_distance = options[:max_distance]
+    raise_exception(:max_distance, @max_distance) unless check_entity(@max_distance)
+  end
+
+  def call(distance)
+    raise_exception(:distance, distance) unless check_entity(distance)
+    distance <= @max_distance
+  end
+
+  private
+
+  def check_entity(value)
+    value.is_a?(Numeric) && value >= 0
+  end
+
+  def raise_exception(entity, value)
+    raise ArgumentError, "#{entity} should be a non-negative number, got #{value}:#{value.class}"
+  end
 end
