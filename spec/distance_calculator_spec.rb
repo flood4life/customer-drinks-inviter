80705620f81f00d7c04cbd2eb48e3b7d2c6edd67
Murad Yusufov <flood4life@gmail.com>
2017-12-25 08:35:39 +0300

    Fix Moscow's longitude in the spec, add London to Dublin spec

--- spec/distance_calculator_spec.rb
+++ spec/distance_calculator_spec.rb
@@ -5,15 +5,23 @@ describe DistanceCalculator do
   describe 'when input is correct' do
     it 'returns zero if both points are the same' do
       point = { latitude: 10, longitude: 10 }
-      assert_in_epsilon 0, DistanceCalculator.call(point, point)
+      assert_in_delta 0, DistanceCalculator.call(point, point), 0.1 # use delta here because relative error makes no sense for 0
     end
 
     it 'returns correct distance between Moscow and Dublin' do
       # data from WolframAlpha
-      moscow   = { latitude: 55.75, longitude: 37.62 }
+      moscow   = { latitude: 55.75, longitude: -37.62 }
       dublin   = { latitude: 53.33, longitude: 6.25 }
       distance = 2804e3 # 2804km
-      assert_in_epsilon distance, DistanceCalculator.call(moscow, dublin)
+      assert_in_epsilon distance, DistanceCalculator.call(moscow, dublin), 0.01
+    end
+
+    it 'returns correct distance between London and Dublin' do
+      # data from WolframAlpha
+      london   = { latitude: 51.5, longitude: 0.1167 }
+      dublin   = { latitude: 53.33, longitude: 6.25 }
+      distance = 464.1e3 # 464.1km
+      assert_in_epsilon distance, DistanceCalculator.call(london, dublin), 0.01
     end
   end
 
