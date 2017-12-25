2759d790785700fc159795249dbea671ba953a72
Murad Yusufov <flood4life@gmail.com>
2017-12-25 09:23:04 +0300

    Add CustomerParser implementation

--- src/customer_parser.rb
+++ src/customer_parser.rb
@@ -1,2 +1,18 @@
+require 'json'
+
 class CustomerParser
+  class << self
+    def call(json_string)
+      parsed_json = JSON.parse(json_string)
+      # using safe operator &. because CustomerValidator will take care if it's nil
+      {
+        name:      parsed_json['name'],
+        user_id:   parsed_json['user_id']&.to_i,
+        latitude:  parsed_json['latitude']&.to_f,
+        longitude: parsed_json['longitude']&.to_f
+      }
+    rescue JSON::ParserError => e
+      raise ArgumentError, "Not a valid json: #{json_string}"
+    end
+  end
 end
