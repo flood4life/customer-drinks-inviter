1d8735af8d826017133d5337b3be4bf9353400b3
Murad Yusufov <flood4life@gmail.com>
2017-12-25 14:13:33 +0300

    Fix typo in DistanceCalculator and format the file

--- lib/distance_calculator.rb
+++ lib/distance_calculator.rb
@@ -15,10 +15,10 @@ class DistanceCalculator
     EARTH_RADIUS = 6_371e3 # 6371km
 
     def distance
-      sin_product = Math.sin(@lat1) * Math.sin(@lat2)
+      sin_product     = Math.sin(@lat1) * Math.sin(@lat2)
       longitude_delta = (@long1 - @long2).abs
-      cos_product = Math.cos(@lat1) * Math.cos(@lat2) * Math.cos(longitude_delta)
-      central_angle = Math.acos(sin_product + cos_product)
+      cos_product     = Math.cos(@lat1) * Math.cos(@lat2) * Math.cos(longitude_delta)
+      central_angle   = Math.acos(sin_product + cos_product)
       EARTH_RADIUS * central_angle
     end
 
@@ -27,8 +27,8 @@ class DistanceCalculator
     end
 
     def convert_instance_to_radians
-      @lat1 = radians_from_degrees(@lat1)
-      @lat2 = radians_from_degrees(@lat2)
+      @lat1  = radians_from_degrees(@lat1)
+      @lat2  = radians_from_degrees(@lat2)
       @long1 = radians_from_degrees(@long1)
       @long2 = radians_from_degrees(@long2)
     end
@@ -43,8 +43,8 @@ class DistanceCalculator
     def validate_instance
       raise_exception(1, :latitude, @lat1) unless latitude_check(@lat1)
       raise_exception(2, :latitude, @lat2) unless latitude_check(@lat2)
-      raise_exception(1, :longitude, @long1) unless latitude_check(@long1)
-      raise_exception(2, :longitude, @long2) unless latitude_check(@long2)
+      raise_exception(1, :longitude, @long1) unless longitude_check(@long1)
+      raise_exception(2, :longitude, @long2) unless longitude_check(@long2)
     end
 
     def latitude_check(latitude)
