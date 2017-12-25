95f5150451cd2304cb455c5adf075cf30dbd13cc
Murad Yusufov <flood4life@gmail.com>
2017-12-25 10:19:58 +0300

    Add FileReader implementation

--- src/file_reader.rb
+++ src/file_reader.rb
@@ -1,2 +1,23 @@
 class FileReader
+  class FileReader::Error < StandardError
+  end
+
+  class FileReader::PermissionError < FileReader::Error
+  end
+
+  def initialize(filename)
+    @file = File.open(filename, 'r')
+  rescue Errno::EACCES => e
+    raise FileReader::PermissionError, e.message
+  rescue Errno::ENOENT => e
+    raise ArgumentError, "File with a name `#{filename}` is not present"
+  rescue IOError => e
+    raise FileReader::Error, e.message
+  end
+
+  def each_line
+    @file.each_line do |line|
+      yield line.chomp
+    end
+  end
 end
